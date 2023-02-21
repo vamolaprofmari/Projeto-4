e81b075fe649ee437f95e9509e7cb2b1b3c80f5f
vamolaprofmari <123978185+vamolaprofmari@users.noreply.github.com>
2023-02-21 17:20:06 -0300

    Update sketch.js
    estava basicamente tudo certo, mas aproveitei para otimizar os casos do balão, pois você criou a função la embaixo, então era preciso apenas chama-las. Atenção ao copiar e colar, pois você não trocou as imagens dos balões, mesma coisa no projeto anterior. Atenção também em colocar as imagens com o nome certo, pois se a imagem não for localizada, o código não roda.

--- C14-project-template/sketch.js
+++ C14-project-template/sketch.js
@@ -10,11 +10,11 @@ function preload(){
   bowImage = loadImage("bow0.png");
   red_balloonImage = loadImage("red_balloon0.png");
   // carregue a imagem do balão verde
-  green_balloonImage = loadImage("green_ballon0.png")
+  green_balloonImage = loadImage("green_balloon0.png")
   // carregue a imagem do balão rosa
-  pink_balloonImage = loadImage("pink_ballon0.png")
+  pink_balloonImage = loadImage("pink_balloon0.png")
   // carregue a imagem do balão azul
-  blue_balloonImage = loadImage("blue_ballon0.png")
+  blue_balloonImage = loadImage("blue_balloon0.png")
   
 }
 
@@ -55,36 +55,24 @@ function draw() {
   }
    
   //criando inimigos continuamente
-  var select_ballon = Math.round (ramdom(1,4));
+  var select_balloon = Math.round (random(1,4));
   
   if (World.frameCount % 100 == 0) {
    switch(select_balloon ){
-    case 1: red = createSprite(0,Math.round(random(20, 370)), 10, 10);
-    red.addImage(red_balloonImage);
-    red.velocityX = 3;
-    red.lifetime = 150;
-    red.scale = 0.1;
+    case 1: 
+       redBalloon();
     break;
     
-    case 2: blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
-    blue.addImage(red_balloonImage);
-    blue.velocityX = 3;
-    blue.scale = 0.1;
-    blue.lifetime = 150;
+    case 2: 
+       blueBalloon();
     break;
     
-    case 3: green = createSprite(0,Math.round(random(20, 370)), 10, 10);
-    green.addImage(red_balloonImage);
-    green.velocityX = 3;
-    green.scale = 0.1;
-    green.lifetime = 150; 
+    case 3: 
+       greenBalloon();
     break;
     
-    case 4: pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
-    pink.addImage(red_balloonImage);
-    pink.velocityX = 3;
-    pink.scale = 0.1;
-    pink.lifetime = 150;
+    case 4: 
+       pinkBalloon();
     break;
     default:break;
   }}
@@ -116,7 +104,7 @@ function blueBalloon() {
   //crieSprite para o balão
   var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
   //adicioneImagem para o balão
-  blue.addImage(red_balloonImage);
+  blue.addImage(blue_balloonImage);
   // adicione velocidade para fazer o balão se mover
   blue.velocityX = 3;
   // mudar a dimensão do balão
@@ -129,7 +117,7 @@ function greenBalloon() {
   //crieSprite para o balão
   green = createSprite(0,Math.round(random(20, 370)), 10, 10);
   //adicioneImagem para o balão
-  green.addImage(red_balloonImage);
+  green.addImage(green_balloonImage);
   // adicione velocidade para fazer o balão se mover
   green.velocityX = 3;
   // mudar a dimensão do balão
@@ -142,7 +130,7 @@ function pinkBalloon() {
  //crieSprite para o balão
  var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
   //adicioneImagem para o balão
-  pink.addImage(red_balloonImage);
+  pink.addImage(pink_balloonImage);
   // adicione velocidade para fazer o balão se mover
   pink.velocityX = 3;
   // mudar a dimensão do balão
